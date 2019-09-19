f534d9780806f23659c594b4fd51b3228a626df5
csinrn <csinrn24@gmail.com>
2019-09-19 22:49:07 +0800

    feat: delete userlens in mariadb

--- lens.sql
+++ lens.sql
@@ -24,15 +24,12 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`account`),
   UNIQUE KEY `account` (`account`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table lensdb.admin: ~4 rows (approximately)
+-- Dumping data for table lensdb.admin: ~1 rows (approximately)
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 INSERT INTO `admin` (`account`, `password`, `create_at`, `name`) VALUES
-	('qwe', '$2a$10$.881muRUU5c2uCPeTbif0OQvHpKIbvjncUTWNDz89MCav3.e6Aslu', '2019-08-29', 'qwe'),
-	('rrr', '$2a$10$CL41ykQffv5WnQ6o8.tRh.1IqUgXkLOmo6wdMosXOwa3iR15w8yFK', '2019-09-07', 'rrr'),
-	('string', '$2a$10$OVL/wwHL/kpKKFi/58G4meLD3RyauYkzxUg/IzZ.FqWAdfBQuLa9u', '2019-03-03', 'string'),
-	('string777', '$2a$10$I4iLe8ZcBopTsmUkz6hYkeCCrmVlg0wbPKrjEpbXX.yhPxJfVSrUO', '2019-08-05', 'string');
+	('qwe', '$2a$10$.881muRUU5c2uCPeTbif0OQvHpKIbvjncUTWNDz89MCav3.e6Aslu', '2019-08-29', 'qwe');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 
 -- Dumping structure for table lensdb.lens
@@ -63,9 +60,8 @@ CREATE TABLE IF NOT EXISTS `lens` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `no` (`no`)
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE lens CONVERT TO CHARACTER SET UTF8;
 -- Dumping data for table lensdb.lens: ~9 rows (approximately)
 /*!40000 ALTER TABLE `lens` DISABLE KEYS */;
 INSERT INTO `lens` (`id`, `name`, `no`, `diameter`, `BC`, `powerL`, `powerH`, `water`, `daily`, `biweekly`, `monthly`, `place_of_prod`, `price`, `package`, `special_price`, `event_disp`, `license`, `new_tag`, `hotsale_tag`, `onsale_tag`, `create_at`, `update_at`, `url`) VALUES
@@ -79,22 +75,6 @@ INSERT INTO `lens` (`id`, `name`, `no`, `diameter`, `BC`, `powerL`, `powerH`, `w
 	(45, '焦糖布蕾', 5, 20, 6, 100, 200, 10, 1, 0, 0, '日本', 700, 20, NULL, NULL, 'B34567', 0, 1, 0, '2019-09-13', NULL, '/lensPic/焦糖布蕾.png'),
 	(46, '幕斯可可', 8, 13, 10, 50, 100, 20, 1, 0, 0, '北極', 3000, 3, NULL, NULL, 'K123456', 0, 1, 0, '2019-09-13', NULL, '/lensPic/幕斯可可.png');
 /*!40000 ALTER TABLE `lens` ENABLE KEYS */;
-
--- Dumping structure for table lensdb.userlens
-CREATE TABLE IF NOT EXISTS `userlens` (
-  `user_id` int(11) NOT NULL DEFAULT 0,
-  `lens_id` int(11) NOT NULL DEFAULT 0,
-  `lens_count` int(11) NOT NULL,
-  `lens_time` int(11) DEFAULT NULL,
-  `create_at` date NOT NULL,
-  `update_at` date DEFAULT NULL,
-  KEY `lens_id` (`lens_id`),
-  CONSTRAINT `lens_id` FOREIGN KEY (`lens_id`) REFERENCES `lens` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table lensdb.userlens: ~0 rows (approximately)
-/*!40000 ALTER TABLE `userlens` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userlens` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
