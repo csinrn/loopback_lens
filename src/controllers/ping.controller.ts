8b4e1e08e301a513941a7141947ba797981f9b25
Timmatt <timmatt@ntu.im>
2019-10-31 10:28:56 +0800

    test: time zone

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -48,12 +48,11 @@ export class PingController {
     // Reply with a greeting, the current time, the url, and request headers
     var updateTime = await this.updateTimeRepository.findById('0');
     var dt = new Date()
-    dt = new Date()
-    var hour = dt.getHours() - 8
+    var hour = dt.getHours()
     var isUpdateTime = updateTime.updateFrom <= hour && updateTime.updateTo > hour
     return {
       greeting: 'Hello from LoopBack',
-      date: dt.toLocaleString(),
+      date: dt,
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
       serverIp: '192.168.1.109',
